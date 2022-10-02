2fcf01a3bde844d81b6e7f7493baeb440aff4ec9
Rakta1 <Rakta0613@gmail.com>
2022-10-02 11:13:15 -0400

    included WeatherIcon and animated weather icons

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ function App() {
 			<div className="container">
 				<form>
 					<div className="row">
-						<div className="col-9">
+						<div className="col-7">
 							<input
 								type="search"
 								placeholder="Enter a city"
 								className="form-control"
+								autoFocus
 							/>
 						</div>
 						<div className="col-3">
-							<input type="submit" value="Search" className="btn btn-primary" />
+							<input
+								type="submit"
+								value="Search"
+								className="btn btn-primary w-100"
+							/>
 						</div>
 					</div>
 				</form>
+				<h1>New York</h1>
 			</div>
 		</div>
 	);
